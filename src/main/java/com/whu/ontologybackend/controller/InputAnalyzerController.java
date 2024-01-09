93f9c8715acaf66a8fc0f1795da51bf8d0b2b9a1
gintama <1198582189@qq.com>
2024-01-09 21:59:59 +0800

    dispatch

--- src/main/java/com/whu/ontologybackend/controller/InputAnalyzerController.java
+++ src/main/java/com/whu/ontologybackend/controller/InputAnalyzerController.java
@@ -1,15 +1,21 @@
 package com.whu.ontologybackend.controller;
 
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.Map;
 
 @RestController
 @RequestMapping("inputAnalyzer")
 public class InputAnalyzerController {
+    private static String resourcePrefix = "D:\\Lowcode-ontology-generator\\Lowcode-ontology-generator-backend\\backend\\src\\main\\resources\\multi-source\\";
+
     @RequestMapping("postTest")
     public String postTest(@RequestParam("name") String name, @RequestParam("age") Integer age){
         return "name: " + name + " age: " + age + "... OK";
@@ -47,6 +53,16 @@ public class InputAnalyzerController {
 
     @RequestMapping("postOntology")
     public String integrateOntology(){
+        File ontologyFile = new File(resourcePrefix + "MALOnt.owl");
+        // move specific process away from controllers
+        try{
+            InputStream inputStream = new FileInputStream(ontologyFile);
+            OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
+            m.read(inputStream, "utf-8"); // second parameter "base"
+            return "open ontology: " + m.getBaseModel().toString();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         return "ontology";
     } // process existing ontologies
 
@@ -55,6 +71,7 @@ public class InputAnalyzerController {
         if(!file.isEmpty()){
             try{
                 byte[] bytes = file.getBytes();
+                // for example, process ontology files
                 return "redirect:/success";
             } catch (Exception e){
                 return "redirect:/error";
