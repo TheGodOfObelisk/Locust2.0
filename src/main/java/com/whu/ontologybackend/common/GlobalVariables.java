5030c4e851f2a4a5007604d7ab0732f547c8ed9d
gintama <1198582189@qq.com>
2024-02-23 00:00:32 +0800

    glossary

--- src/main/java/com/whu/ontologybackend/common/GlobalVariables.java
+++ src/main/java/com/whu/ontologybackend/common/GlobalVariables.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Component
 public class GlobalVariables {
+
     public static List<OntMultiwayTree> ontMultiwayForest = new ArrayList<>();
 }
---
+++ src/main/java/com/whu/ontologybackend/common/structured/Glossary.java
@@ -0,0 +1,32 @@
+package com.whu.ontologybackend.common.structured;
+
+public class Glossary {
+    private String word;
+    private String description;
+
+    private String label;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
