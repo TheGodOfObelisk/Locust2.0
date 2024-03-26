14aeb6ee66d259b0d9810aca602434fd42df2660
gintama <1198582189@qq.com>
2024-03-26 21:33:00 +0800

    extract the results of pyate into a map

--- src/main/java/com/whu/ontologybackend/common/utils/ExternalCallOperationMethods.java
+++ src/main/java/com/whu/ontologybackend/common/utils/ExternalCallOperationMethods.java
@@ -1,8 +1,13 @@
 package com.whu.ontologybackend.common.utils;
 
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ExternalCallOperationMethods {
     public static void callPythonScript(String path) throws IOException, InterruptedException {
@@ -34,14 +39,25 @@ public class ExternalCallOperationMethods {
         StringBuilder output = new StringBuilder();
         String line;
 
+        String regex = "\\s{2,}";
+        Map<String, Double> extractedTerms = new HashMap<>();
         while((line = reader.readLine()) != null){
+            String[] parts = line.split(regex);
+            if(parts.length != 2){
+                System.out.println("Error occurred or meets the end!");
+            } else {
+                String key = parts[0];
+                Double value = Double.parseDouble(parts[1]);
+                extractedTerms.put(key, value);
+            }
             output.append(line).append("\n");
+//            System.out.println(parts);
         }
 
         int exitCode = process.waitFor();
-
-        System.out.println("Exit code: " + exitCode);
-        System.out.println("Output: \n" + output.toString());
+        System.out.println(extractedTerms.toString());
+//        System.out.println("Exit code: " + exitCode);
+//        System.out.println("Output: \n" + output.toString());
 
         return;
     }
