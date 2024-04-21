28c301d8e80b975f00ce91ab49896775a6bcacb0
gintama <1198582189@qq.com>
2024-04-21 21:27:27 +0800

    extract results into jsonarray

--- src/main/java/com/whu/ontologybackend/service/impl/InputAnalyzeServiceImpl.java
+++ src/main/java/com/whu/ontologybackend/service/impl/InputAnalyzeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.whu.ontologybackend.service.impl;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONReader;
 import com.whu.ontologybackend.common.utils.CommonOperationMethods;
@@ -23,6 +25,8 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 
@@ -142,7 +146,19 @@ public class InputAnalyzeServiceImpl implements InputAnalyzeService {
 
         try{
             ExternalCallOperationMethods.callPythonScript(openIEScriptPath + "\\tripleExtractionWithOpenIE.py");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
+        String jsonFile = openIEScriptPath + "\\triples.json";
+        JSONArray jsonArray = new JSONArray();
+        try{
+            String jsonContent = new String(Files.readAllBytes(Paths.get(jsonFile)));
+            jsonArray = JSON.parseArray(jsonContent);
+            for(int i = 0; i < jsonArray.size(); i++){
+                JSONObject item = jsonArray.getJSONObject(i);
+                System.out.println(item.toString());
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
---
+++ target/classes/static/externalScripts/tripleExtractionWithOpenIE.py
@@ -0,0 +1,36 @@
+from openie import StanfordOpenIE
+import json
+
+# https://stanfordnlp.github.io/CoreNLP/openie.html#api
+# Default value of openie.affinity_probability_cap was 1/3.
+properties = {
+    'openie.affinity_probability_cap': 2 / 3,
+}
+
+with StanfordOpenIE(properties=properties) as client:
+    text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
+    print('Text: %s.' % text)
+    for triple in client.annotate(text):
+        print('|-', triple)
+
+    # graph_image = 'graph.png'
+    # client.generate_graphviz_graph(text, graph_image)
+    # print('Graph generated: %s.' % graph_image)
+
+    # with open('pg6130.txt', encoding='utf8') as r:
+    #     corpus = r.read().replace('\n', ' ').replace('\r', '')
+    with open('D:\\researchPro\\testStanfordNLP\\tmpArticle.txt', encoding='utf8') as r:
+        corpus = r.read().replace('\n', ' ').replace('\r', '')
+
+    triples_corpus = client.annotate(corpus[0:5000])
+    print('Corpus: %s [...].' % corpus[0:80])
+    print('Found %s triples in the corpus.' % len(triples_corpus))
+    print(type(triples_corpus).__name__)
+    for triple in triples_corpus:
+        print('|-', triple)
+    with open('D:\\researchPro\\testStanfordNLP\\triples.json', 'w') as f:
+        json.dump(triples_corpus, f)
+    # for triple in triples_corpus[:3]:
+    #     print('|-', triple)
+    #     print(type(triple).__name__)
+    print('[...]')
