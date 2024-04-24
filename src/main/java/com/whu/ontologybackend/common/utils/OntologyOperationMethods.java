96ea438e9e8f6ede8d7e39ef489e8304f70f2584
gintama <1198582189@qq.com>
2024-04-24 21:45:38 +0800

    task 1 start

--- src/main/java/com/whu/ontologybackend/common/utils/OntologyOperationMethods.java
+++ src/main/java/com/whu/ontologybackend/common/utils/OntologyOperationMethods.java
@@ -893,6 +893,8 @@ public class OntologyOperationMethods {
     }
 
     private static void updateOntologyForestByConceptGlossary(Glossary glossary){
+        // it has been check that no duplicated glossary is in the ontology forest
+        List<TripleRecord> tripleRecordList = new ArrayList<>();
 
     }
 
