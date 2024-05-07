98449eeba774338f988082f809b9362217229d7d
gintama <1198582189@qq.com>
2024-05-07 20:07:47 +0800

    complete SPARQL query test

--- src/main/java/com/whu/ontologybackend/controller/JenaTestController.java
+++ src/main/java/com/whu/ontologybackend/controller/JenaTestController.java
@@ -1,15 +1,16 @@
 package com.whu.ontologybackend.controller;
 
 import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.iri.impl.Main;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.query.Dataset;
-import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.VCARD;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,18 +52,40 @@ public class JenaTestController {
 
     @RequestMapping("fusekiTest")
     public String JenaFuseki(){
-        File ontologyFile = new File("D:\\cybersecurityOntologies\\CAPEC.owl");
+        // error occurred
+//        File ontologyFile = new File("D:\\cybersecurityOntologies\\CAPEC.owl");
+        File ontologyFile = new File("D:\\ontologies\\killchain.owl"); //test existing ontology
         try{
             InputStream inputStream = new FileInputStream(ontologyFile);
+            FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
+            Model model = FileManager.get().loadModel("D:\\ontologies\\killchain.owl");
             OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
             ontModel.read(inputStream, "utf-8");
-            Dataset dataset = DatasetFactory.create(ontModel);
+//            Dataset dataset = DatasetFactory.create(ontModel);
+            Dataset dataset = DatasetFactory.createTxnMem();
             FusekiServer server = FusekiServer.create().add("/dataset", dataset).build();
             server.start();
+            // refer to https://hydrargillite4.rssing.com/chan-3685425/all_p13.html
+            String queryString =
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n " +
+                    "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n " +
+                    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n " +
+                    "SELECT ?subject ?object \n" +
+                    "\tWHERE { ?subject rdfs:subClassOf ?object }";
+            Query query = QueryFactory.create(queryString);
+            QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
+//            QueryExecution queryExecution = QueryExecution.create("SELECT * { ?s  ?o}", dataset);
+            ResultSet rs = queryExecution.execSelect();
+            ResultSetFormatter.out(rs);
+
+            System.out.println("SPARQL query ends. Stop the fuseki server.");
+            queryExecution.close();
+            server.stop();
         } catch (Exception e){
             e.printStackTrace();
         }
-//        Dataset dataset = DatasetFactory.createTxnMem();
+
 
 
 //        server.stop();
